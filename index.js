c892f63c22c0f875b512ae8ff51f2e7b7109ddac
MD Harunar Rashid <harunarrashid4093@gmail.com>
2025-09-09 18:21:41 +0600

    commit my repo

--- index.js
+++ index.js
@@ -9,20 +9,53 @@ const plantDetails = () => {
     const url = `https://openapi.programming-hero.com/api/category/${id}`;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => displayLevelcard(data.data));
+      .then((data) => displayLevelcard(data.plants));
   };
   
+  // {
+        //     "id": 3,
+        //     "image": "https://i.ibb.co.com/xt98PwZq/jackfruit-min.jpg",
+        //     "name": "Jackfruit Tree",
+        //     "description": "A large tropical tree that bears the world’s biggest fruit, the jackfruit. Its sweet and aromatic flesh is both nutritious and filling, and the tree itself provides generous shade.",
+        //     "category": "Fruit Tree",
+        //     "price": 800
+        // }
+
   const displayLevelcard = (cards) => {
+    console.log(cards);
     const cardContainer = document.getElementById("card-container")
     cardContainer.innerHTML = "";
 
     cards.forEach(card =>{
         console.log(card);
         const cardFile = document.createElement("div");
-        card.innerHTML = `
+        cardFile.innerHTML = `
+      
         
-        
-        `
+    <div class="card bg-white ">
+    <div>
+        <figure class=" p-4">
+            <img class="w-[100%] h-48 object-cover rounded-md" 
+              src="${card.image}"
+              alt="Shoes"
+              class="rounded-xl" />
+          </figure>
+          <div class="card-body ">
+            <h2 class="card-title font-semibold text-[14px]">${card.name}</h2>
+            <p class="text-[12px]">${card.description} </p>
+            <div class=" flex justify-between">
+              <h1 class=" text-[14px] px-2 py-1 rounded-2xl text-[#15803D] bg-[#DCFCE7] ">${card.category}</h1>
+             <div class=" font-semibold"> <p>${card.price}</p></div>
+            </div>
+            <div class="card-actions">
+              <button class="btn btn-primary w-full rounded-full">Add to Card</button>
+            </div>
+          
+        </div>
+    </div>
+  </div>
+        `;
+        cardContainer.append(cardFile);
     });
 
     };
