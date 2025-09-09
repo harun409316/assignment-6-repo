c02e98f31dc3de19b5111384cce3fec0e249475f
MD Harunar Rashid <harunarrashid4093@gmail.com>
2025-09-09 15:14:21 +0600

    commit my assign

--- index.js
+++ index.js
@@ -1,45 +1,58 @@
 const plantDetails = () => {
     fetch("https://openapi.programming-hero.com/api/categories")
-    .then((res) => res.json())
-    .then((json) => displayLoadplant(json.categories));
-};
+      .then((res) => res.json())
+      .then((json) => displayLoadplant(json.categories));
+  };
+  
+  const loadLevelCard = (id) => {
+    console.log(id);
+    const url = `https://openapi.programming-hero.com/api/category/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayLevelcard(data.data));
+  };
+  
+  const displayLevelcard = (cards) => {
+    const cardContainer = document.getElementById("card-container")
+    cardContainer.innerHTML = "";
 
- const loadLevelCard = (id) => {
-console.log(id);
-//     const url = `https://openapi.programming-hero.com/api/plants${id}`
-// fetch(url)
-// .then(res => res.json())
-// .then(data => console.log(data));
+    cards.forEach(card =>{
+        console.log(card);
+        const cardFile = document.createElement("div");
+        card.innerHTML = `
+        
+        
+        `
+    });
 
- };
- 
-
-const displayLoadplant = (categories) => {
+    };
+  
+  const displayLoadplant = (categories) => {
     console.log(categories);
-    // 1 get the container 
+    // 1 get the container
     const leftSide = document.getElementById("left-side");
     leftSide.innerHTML = "";
-console.log(categories);
-    // 2. get into every plants 
-for(let category of categories){
-    console.log(category);
-    // 3. create Element 
-    const btnDiv = document.createElement("div");
-    btnDiv.innerHTML = `
-    
-    <button onclick="loadLevelCard('${category.id}') class="btn btn-soft btn-accent">${category.category_name}</button>
-    
-    
-    `;
-    leftSide.append(btnDiv);
-}
-
-//     const url = "https://openapi.programming-hero.com/api/categories";
-//     fetch(url)
-//     .then(res=>res.json())
-//     .then(json => console.log(json));
-// }
-};
-
-// displayLoadplant();
-plantDetails();
+    console.log(categories);
+    // 2. get into every plants
+    for (let category of categories) {
+      console.log(category);
+      // 3. create Element
+      const btnDiv = document.createElement("div");
+      btnDiv.innerHTML = `
+      
+      <button onclick="loadLevelCard('${category.id}')" class="btn btn-soft btn-accent">${category.category_name}</button>
+      
+      
+      `;
+      leftSide.append(btnDiv);
+    }
+  
+    //     const url = "https://openapi.programming-hero.com/api/categories";
+    //     fetch(url)
+    //     .then(res=>res.json())
+    //     .then(json => console.log(json));
+    // }
+  };
+  
+ 
+  plantDetails();
